c7ce77e31839c3891f4371efa2f1f006f7d283f6
Borjaarmet <borjaarmet@gmail.com>
2021-10-06 15:31:53 +0200

    change some styles

--- js/main.js
+++ js/main.js
@@ -24,6 +24,16 @@ const navSlide = () => {
 
 navSlide();
 
+//button info
+
+function hideBtn() {
+  const btnInfo = document.querySelector(".btn-info");
+  btnInfo.addEventListener("click", () => {
+    btnInfo.style.display = "none";
+  });
+}
+hideBtn();
+
 // button language
 
 const check = document.querySelector(".check");
